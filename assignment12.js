67b2b50ca79d6374594d2065e1cd6d0864eef76b
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 13:24:40 +0530

    Refactor bank transaction analysis code

--- assignment12.js
+++ assignment12.js
@@ -1,41 +1,40 @@
 /*
 Bank Transaction Analyzer
-
 You are building a bank statement summary.
 Test data:
+transactions = [
+  { id: 1, type: "credit", amount: 5000 },
+  { id: 2, type: "debit", amount: 2000 },
+  { id: 3, type: "credit", amount: 10000 },
+  { id: 4, type: "debit", amount: 3000 }
+];
+*/
+
 const transactions = [
   { id: 1, type: "credit", amount: 5000 },
   { id: 2, type: "debit", amount: 2000 },
   { id: 3, type: "credit", amount: 10000 },
   { id: 4, type: "debit", amount: 3000 }
 ];
-Tasks:
-    1. filter() all credit transactions
-    2. map() to extract only transaction amounts
-    3. reduce() to calculate final account balance
-    4. find() the first debit transaction
-    5. findIndex() of transaction with amount 10000
-    */
-const transactions = [
-  { id: 1, type: "credit", amount: 5000 },
-  { id: 2, type: "debit", amount: 2000 },
-  { id: 3, type: "credit", amount: 10000 },
-  { id: 4, type: "debit", amount: 3000 }
-];
 
-let q1=transactions.filter(x=>x.type=='credit');
-console.log(q1);
+//filter() all credit transactions
+let res51=transactions.filter(x=>x.type=='credit');
+console.log(res51);
 
-let q2=transactions.map(x=> x.amount)
-console.log(q2);
+//map() to extract only transaction amounts
+let res52=transactions.map(x=> x.amount)
+console.log(res52);
 
-let q3=transactions.reduce((acc,x)=>{
+//reduce() to calculate final account balance
+let res53=transactions.reduce((acc,x)=>{
     return (x.type=='credit')?acc+x.amount:acc-x.amount;
 },0);
-console.log(q3);
+console.log(res53);
 
-let q4=transactions.find(x=>x.type=='debit');
-console.log(q4);
+ //find() the first debit transaction
+let res54=transactions.find(x=>x.type=='debit');
+console.log(res54);
 
-let q5=transactions.findIndex(x=>x.amount==10000);
-console.log(q5);
+//findIndex() of transaction with amount 10000
+let res55=transactions.findIndex(x=>x.amount==10000);
+console.log(res55);
