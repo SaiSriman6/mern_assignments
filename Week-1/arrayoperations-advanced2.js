580a96d3b350abff5e6cb0d34a3ff37d901801c6
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 12:43:53 +0530

    Add arrayoperations-advanced2.js file

--- Week-1/arrayoperations-advanced2.js
+++ Week-1/arrayoperations-advanced2.js
@@ -1,12 +1,14 @@
-//filter() students who passed (marks ≥ 40)
-//map() to add a grade field
- /*       ≥90 → A
-        ≥75 → B
-        ≥60 → C
-        else → D*/
-//reduce() to calculate average marks
-//find() the student who scored 92
-//findIndex() of student "Kiran"
+//Student Performance Dashboard
+//You are working on a college result analysis system.
+//Test Data:
+/*students = [
+  { id: 1, name: "Ravi", marks: 78 },
+  { id: 2, name: "Anjali", marks: 92 },
+  { id: 3, name: "Kiran", marks: 35 },
+  { id: 4, name: "Sneha", marks: 88 },
+  { id: 5, name: "Arjun", marks: 40 }
+];*/
+
 
 const students = [
   { id: 1, name: "Ravi", marks: 78 },
@@ -16,9 +18,15 @@ const students = [
   { id: 5, name: "Arjun", marks: 40 }
 ];
 
+//filter() students who passed (marks ≥ 40)
 let res51=students.filter(x=>x.marks>=40)
 console.log(res51)
 
+//map() to add a grade field
+ /*       ≥90 → A
+        ≥75 → B
+        ≥60 → C
+        else → D*/
 let res52=students.map(x=>{
     if(x.marks>=90){
         x.grade='A'
@@ -36,14 +44,14 @@ let res52=students.map(x=>{
 })
 console.log(res52)
 
+//reduce() to calculate average marks
 let res53=students.reduce((acc,x)=>acc+x.marks,0)/students.length
 console.log(res53)
 
+//find() the student who scored 92
 let res54=students.find((x)=>x.marks==92);
 console.log(res54);
 
+//findIndex() of student "Kiran"
 let res55=students.findIndex((x)=>x.name=='kiran');
-console.log(res55);
-
-let res56 = students.findIndex(x => x.name === "Kiran");
 console.log(res55);
