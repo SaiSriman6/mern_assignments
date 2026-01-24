c267c3aa951cff138d74d47deec542297d28f17e
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 12:39:38 +0530

    Update and rename assignment8.js to Week-1/arrayoperations-advanced1.js

--- Week-1/arrayoperations-advanced1.js
+++ Week-1/arrayoperations-advanced1.js
@@ -1,9 +1,18 @@
+//You are building a shopping cart summary for an e-commerce website.
+//Test Data : 
+/*cart = [
+  { id: 101, name: "Laptop", price: 60000, quantity: 1, inStock: true },
+  { id: 102, name: "Mouse", price: 800, quantity: 2, inStock: true },
+  { id: 103, name: "Keyboard", price: 1500, quantity: 1, inStock: false },
+  { id: 104, name: "Monitor", price: 12000, quantity: 1, inStock: true }
+];*/
 const cart = [
   { id: 101, name: "Laptop", price: 60000, quantity: 1, inStock: true },
   { id: 102, name: "Mouse", price: 800, quantity: 2, inStock: true },
   { id: 103, name: "Keyboard", price: 1500, quantity: 1, inStock: false },
   { id: 104, name: "Monitor", price: 12000, quantity: 1, inStock: true }
 ];
+
 //Use filter() to get only inStock products
 let res31=cart.filter(x=>x.inStock==true)
 console.log(res31)
