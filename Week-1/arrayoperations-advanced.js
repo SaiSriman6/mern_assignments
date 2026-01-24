259c1b923b719b8b80fb94828e0c47ac80038776
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 12:56:02 +0530

    Rename arrayoperations-advanced1.js to arrayoperations-advanced.js

--- Week-1/arrayoperations-advanced.js
+++ Week-1/arrayoperations-advanced.js
@@ -32,3 +32,44 @@ console.log(res34)
 //Use findIndex() to find the position of "Keyboard"
 let res35=cart.findIndex(x=>x.name=='Keyboard')
 console.log(res35)
+
+
+//Employee Payroll Processor
+//You are building a salary processing module in a company HR app.
+//Test data:
+/*employees = [
+  { id: 201, name: "Amit", salary: 45000, department: "IT" },
+  { id: 202, name: "Neha", salary: 60000, department: "HR" },
+  { id: 203, name: "Rahul", salary: 75000, department: "IT" },
+  { id: 204, name: "Pooja", salary: 30000, department: "Sales" }
+]; */
+const employees = [
+  { id: 201, name: "Amit", salary: 45000, department: "IT" },
+  { id: 202, name: "Neha", salary: 60000, department: "HR" },
+  { id: 203, name: "Rahul", salary: 75000, department: "IT" },
+  { id: 204, name: "Pooja", salary: 30000, department: "Sales" }
+];
+
+//filter() employees from IT department
+let q1=employees.filter((x)=> x.department=='IT');
+console.log(q1);
+
+//map() to add: netSalary = salary + 10% bonus
+let q2=employees.map((x)=>{ 
+       x.netsalary=x.salary+(x.salary*10)/100;
+       return x;
+})
+console.log(q2);
+
+//reduce() to calculate total salary payout
+let q3=employees.reduce((acc,x)=>acc+x.salary,0);
+console.log(q3);
+
+//find() employee with salary 30000
+let q4=employees.find(x=>x.salary==30000);
+console.log(q4);
+
+//findIndex() of employee "Neha"
+let q5=employees.findIndex(x=>x.name=="Neha");
+console.log(q5);
+
