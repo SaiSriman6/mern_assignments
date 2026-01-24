cee3881fc9af17e9efd71a2e7b258a4c3e9487f9
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 13:09:21 +0530

    Add course price tag labeling based on price

--- Week-1/control-statements1.js
+++ Week-1/control-statements1.js
@@ -19,3 +19,25 @@ else{
     msg+="Welcome back"   //storing result into msg
 }
 console.log(msg)    // Print the message
+
+
+//Course Price Tag Labeler
+//Initial data:
+//price = 1299;
+let price=1299;
+let courseTag=""  //   4. To Store label in courseTag
+
+//1. If price < 500 → "Budget Course"
+if(price<500){
+    courseTag+="Budget Course"
+}
+//2. If price between 500–1000 → "Standard Course"
+else if(price>500 && price<1000){
+    courseTag+="standardcourse"
+}
+//3. If price > 1000 → "Premium Course"
+else{
+    courseTag+="premium Course"
+}
+//5. Print the label
+console.log(courseTag)
