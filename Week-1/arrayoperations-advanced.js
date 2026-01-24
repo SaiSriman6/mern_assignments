27d69dabc80f52a75f187c8b81fe64589352b5f3
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-24 13:02:10 +0530

    Refactor variable names for clarity in array operations

--- Week-1/arrayoperations-advanced.js
+++ Week-1/arrayoperations-advanced.js
@@ -14,24 +14,24 @@ const cart = [
 ];
 
 //Use filter() to get only inStock products
-let res31=cart.filter(x=>x.inStock==true)
-console.log(res31)
+let res11=cart.filter(x=>x.inStock==true)
+console.log(res11)
 
 //Use map() to create a new array with:  { name, totalPrice }
-let res32=cart.map(x=> [x.name,x.price*x.quantity])
-console.log(res32)
+let res12=cart.map(x=> [x.name,x.price*x.quantity])
+console.log(res12)
 
 //Use reduce() to calculate grand total cart value
-let res33=cart.reduce((acc,x)=>acc+x.quantity+x.price,0)
-console.log(res33)
+let res13=cart.reduce((acc,x)=>acc+x.quantity+x.price,0)
+console.log(res13)
 
 //Use find() to get details of "Mouse"
-let res34=cart.find(x=>x.name=='Mouse')
-console.log(res34)
+let res14=cart.find(x=>x.name=='Mouse')
+console.log(res14)
 
 //Use findIndex() to find the position of "Keyboard"
-let res35=cart.findIndex(x=>x.name=='Keyboard')
-console.log(res35)
+let res15=cart.findIndex(x=>x.name=='Keyboard')
+console.log(res15)
 
 
 //Employee Payroll Processor
@@ -51,25 +51,81 @@ const employees = [
 ];
 
 //filter() employees from IT department
-let q1=employees.filter((x)=> x.department=='IT');
-console.log(q1);
+let res21=employees.filter((x)=> x.department=='IT');
+console.log(res21);
 
 //map() to add: netSalary = salary + 10% bonus
-let q2=employees.map((x)=>{ 
+let res22=employees.map((x)=>{ 
        x.netsalary=x.salary+(x.salary*10)/100;
        return x;
 })
-console.log(q2);
+console.log(res22);
 
 //reduce() to calculate total salary payout
-let q3=employees.reduce((acc,x)=>acc+x.salary,0);
-console.log(q3);
+let res23=employees.reduce((acc,x)=>acc+x.salary,0);
+console.log(res23);
 
 //find() employee with salary 30000
-let q4=employees.find(x=>x.salary==30000);
-console.log(q4);
+let res24=employees.find(x=>x.salary==30000);
+console.log(res24);
 
 //findIndex() of employee "Neha"
-let q5=employees.findIndex(x=>x.name=="Neha");
-console.log(q5);
+let res25=employees.findIndex(x=>x.name=="Neha");
+console.log(res25);
 
+//Student Performance Dashboard
+//You are working on a college result analysis system.
+//Test Data:
+/*students = [
+  { id: 1, name: "Ravi", marks: 78 },
+  { id: 2, name: "Anjali", marks: 92 },
+  { id: 3, name: "Kiran", marks: 35 },
+  { id: 4, name: "Sneha", marks: 88 },
+  { id: 5, name: "Arjun", marks: 40 }
+];*/
+
+const students = [
+  { id: 1, name: "Ravi", marks: 78 },
+  { id: 2, name: "Anjali", marks: 92 },
+  { id: 3, name: "Kiran", marks: 35 },
+  { id: 4, name: "Sneha", marks: 88 },
+  { id: 5, name: "Arjun", marks: 40 }
+];
+
+//filter() students who passed (marks ≥ 40)
+let res31=students.filter(x=>x.marks>=40)
+console.log(res31)
+
+//map() to add a grade field
+ /*       ≥90 → A
+        ≥75 → B
+        ≥60 → C
+        else → D*/
+let res32=students.map(x=>{
+    if(x.marks>=90){
+        x.grade='A'
+    }
+    else if(x.marks>=75&&x.marks<90){
+        x.grade='B'
+    }
+    else if(x.marks>=60 && x.marks<75){
+        x.grade='C'
+    }
+    else{
+        x.grade='D'
+    }
+    return x
+})
+console.log(res32)
+
+//reduce() to calculate average marks
+let res33=students.reduce((acc,x)=>acc+x.marks,0)/students.length
+console.log(res33)
+
+//find() the student who scored 92
+let res34=students.find((x)=>x.marks==92);
+console.log(res34);
+
+//findIndex() of student "Kiran"
+let res35=students.findIndex((x)=>x.name=='kiran');
+console.log(res35);
