bbe14a7383b9d14dd4d57014d6f36a645f00b453
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-25 16:53:41 +0530

    Refactor task.js and add validation imports
    Refactored task functions and imported validation functions.

--- Week-2/todo-app/task.js
+++ Week-2/todo-app/task.js
@@ -1,27 +1,17 @@
-/*
-task.js - Task operations
+//task.js - Task operations
 // TODO: Import validator functions
+                    
 // import { ... } from './validator.js';            
-const tasks = [];            
-// 1. Add new task
-function addTask(title, priority, dueDate) {
-// Validate using imported functions
-// If valid, add to tasks array
-// Return success/error message
-}            
-// 2. Get all tasks
-function getAllTasks() {
-// Return all tasks
-}                   
-// 3. Mark task as complete
-function completeTask(taskId) {
-// Find task and mark as complete
-}
-*/
-
 import {validateTitle,validateDueDate,validatePriority} from "./validator.js";
 
 const tasks=[];
+
+// 1. Add new task
+//function addTask(title, priority, dueDate) {
+// Validate using imported functions
+// If valid, add to tasks array
+// Return success/error message
+//}       
 function addTask(title,priority,dueDate){
   let addmsg=validateTitle(title);
   if(addmsg!=true){
@@ -44,9 +34,21 @@ function addTask(title,priority,dueDate){
   tasks.push(task);
   return "task has been added";
 }
+
+// 2. Get all tasks
+//function getAllTasks() {
+// Return all tasks
+//}    
 function getAllTasks(){
     return tasks;
 }
+
+
+// 3. Mark task as complete
+//function completeTask(taskId) {
+// Find task and mark as complete
+//}
+
 function completeTask(title){
    let task=tasks.find(x=>x.title==title);
    if(!task){
