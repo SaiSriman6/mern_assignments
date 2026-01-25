910611b6e42becae2cc6a80cc3b92c17ff6176b5
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-25 16:41:32 +0530

    Add task management functions with validation
    Implemented task management functions with validation.

--- Week-2/todo-app/task.js
+++ Week-2/todo-app/task.js
@@ -1,1 +1,59 @@
+/*
+task.js - Task operations
+// TODO: Import validator functions
+// import { ... } from './validator.js';            
+const tasks = [];            
+// 1. Add new task
+function addTask(title, priority, dueDate) {
+// Validate using imported functions
+// If valid, add to tasks array
+// Return success/error message
+}            
+// 2. Get all tasks
+function getAllTasks() {
+// Return all tasks
+}                   
+// 3. Mark task as complete
+function completeTask(taskId) {
+// Find task and mark as complete
+}
+*/
 
+import {validateTitle,validateDueDate,validatePriority} from "./validator.js";
+
+const tasks=[];
+function addTask(title,priority,dueDate){
+  let addmsg=validateTitle(title);
+  if(addmsg!=true){
+    return addmsg;
+  }
+  let prioritymsg=validatePriority(priority);
+  if(prioritymsg!=true){
+    return prioritymsg;
+  }
+  let dueDatemsg=validateDueDate(dueDate);
+  if(dueDatemsg!=true){
+    return dueDatemsg;
+  }
+  let task={
+      title,
+      priority,
+      dueDate,
+      completed:false
+  }
+  tasks.push(task);
+  return "task has been added";
+}
+function getAllTasks(){
+    return tasks;
+}
+function completeTask(title){
+   let task=tasks.find(x=>x.title==title);
+   if(!task){
+    return 'invalid task name';
+   }
+   task.completed=true;
+   return "task has been completed";
+}
+
+export {addTask,getAllTasks,completeTask};
