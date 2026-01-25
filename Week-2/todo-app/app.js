87f5e6d3dda870bb5dea7654602537e11d276a75
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-25 16:50:57 +0530

    Initialize app.js with task function tests
    Main application file that imports task functions and tests them.

---
+++ Week-2/todo-app/app.js
@@ -0,0 +1,19 @@
+// app.js - Main application
+// TODO: Import task functions
+
+// import { ... } from './task.js';
+import {addTask,getAllTasks,completeTask} from "./task.js";
+
+// // Test your module system
+// 1. Add some tasks
+console.log(addTask('java','high','23-02-2026'));
+console.log(addTask('python','medium','26-01-2026'))
+
+// 2. Display all tasks
+console.log(getAllTasks());
+
+// 3. Complete a task
+console.log(completeTask('java'));
+
+// 4. Display all tasks again
+console.log(getAllTasks());
