86e24c63b1c9d0f75ed9cdd0b9ca8000ec074721
SaiSriman6 <23eg110a65@anurag.edu.in>
2026-01-28 17:14:10 +0530

    Update cart.js

--- Week-2/E-commerce_Shopping_cart_System/cart.js
+++ Week-2/E-commerce_Shopping_cart_System/cart.js
@@ -1,71 +1,82 @@
- import { getProductById, checkStock } from "./products.js";                         
+  import { getProductById, checkStock } from "./products.js";                         
   let cartItems = [];
                           
   // TODO: Implement these functions
                           
   export function addToCart(productId, quantity) {
-    // 1. Get product details
-    // 2. Check stock availability
-    // 3. Check if product already in cart
-    //    - If yes, update quantity
-    //    - If no, add new item
-    // 4. Return success/error message
-    let res1=getProductById(productId);
-    if(!res1){
+    
+    // Get product details
+    let product=getProductById(productId);
+    if(!product){
       return "product not found";
     }
-    let res2=checkStock(productId,quantity);
-    if(!res2){
+
+    // Check stock availability
+    let stock=checkStock(productId,quantity);
+    if(!stock){
         return "Insufficient stock";
     }
-      let res3=cartItems.find(x=>x.productId==productId);
-if(res3){
-res3.quantity+=quantity;
-}
-else{
-cartItems.push({productId,quantity});
-}
-return "Item added to cart succesfully";
-}                                                    
-export function removeFromCart(productId) {
-cartItems=cartItems.filter(x => x.productId !== productId);
-return "Item removed from cart";
-}     
-export function updateQuantity(productId, newQuantity) {
-// Update quantity of product in cart
-// Check stock before updating
-let product=getProductById(productId);
-if(!product){
-return "item not found";
-}
-if(!checkStock(productId,newQuantity)){
-return "insufficient Stock";
-}
-cartItems = cartItems.map(item =>(item.productId === productId)?{ ...item, quantity: newQuantity }: item);
-return "quantity updated";
-}
+
+    // Check if product already in cart
+    let cartItem=cartItems.find(x=>x.productId==productId);
+    if(cartItem){
+       cartItem.quantity+=quantity;  
+    }
+    else{
+       cartItems.push({productId,quantity}); 
+    }
+    // Return success/error message
+    return "Item added to cart succesfully";
+  }            
+
+  export function removeFromCart(productId) {
+    cartItems=cartItems.filter(item => item.productId !== productId);
+    return "Item removed from cart";
+  }    
+   
+  export function updateQuantity(productId, newQuantity) {
+    // Update quantity of product in cart
+    // Check stock before 
+    let cartItem=cartItems.find(item=>item.productId===productId);
+    if(!cartItem){
+      return "Product is not in cart";
+    }
+
+    let product=getProductById(productId);
+    if(!product){
+      return "item not found";
+    }
+    if(!checkStock(productId,newQuantity)){
+      return "insufficient Stock";
+    }
+    cartItem.quantity=newQuantity;
+    return "quantity updated";
+  }
                           
-export function getCartItems() {
-// Return all cart items with product details
-let res=cartItems.map(item => {
-let product = getProductById(item.productId);
-return {...product,quantity:item.quantity};});
-return res;
+  export function getCartItems() {
+    // Return all cart items with product details
+    let allItems=cartItems.map(item => {
+    let product = getProductById(item.productId);
+    return {
+      ...product,
+      quantity:item.quantity
+    };
+  });
+  return allItems;
+  }
 
-}
-export function getCartTotal() {
-// Calculate total price of all items in cart
-// Return total
-let totalcartcost = cartItems.reduce((total, item) => {
-let product = getProductById(item.productId);
-return total+product.price*item.quantity;
-},0);
-return totalcartcost;
-}
+  export function getCartTotal() {
+    // Calculate total price of all items in cart
+    // Return total
+    let totalcartcost = cartItems.reduce((total, item) => {
+    let product = getProductById(item.productId);
+    return total+product.price*item.quantity; 
+  },0);
+    return totalcartcost;
+  }
 
-
-export function clearCart() {
-// Empty the cart
-cartItems=[];
-return "CartCleared";
-}
+  export function clearCart() {
+    // Empty the cart
+    cartItems=[];
+    return "CartCleared";
+  }
